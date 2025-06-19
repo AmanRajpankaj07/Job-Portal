bf9587cffc4907e5276281a19e3ef967f4cb1b45
Aman Raj <amanraj56369509@gmail.com>
2025-06-19 22:33:52 +0530

    url changes

--- Backend/index.js
+++ Backend/index.js
@@ -7,11 +7,14 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
 
 dotenv.config({});
 
+
 const app = express();
 
+const __dirname = path.resolve();
 
 
 // // middleware
@@ -19,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://job-bharat.onrender.com',
     credentials:true
 }
 
@@ -40,6 +43,10 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (_,res) => {
+    res.sendFile(path.resolve(__dirname, "frontend","dist","index.html"));
+})
 
 
 app.listen(PORT,()=>{
